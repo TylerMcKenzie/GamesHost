88dc091829f3583f442e4e5e6314bcfdfc7c3b05
tylermckenzie <tylrmckenz@gmail.com>
2017-09-25 09:50:00 -0500

    adding shape classes

--- app/javascript/BraveEngine/objects/Graphic.js
+++ app/javascript/BraveEngine/objects/Graphic.js
@@ -1,3 +1,27 @@
-export default class Graphic {
-  constructor() {}
+class Graphic {
+  constructor({x = 0, y = 0, color = "black", shape = "rect", context = null, draw = this._draw, update = this._update}) {
+    this._shape = shape
+
+    this.color = color
+    this.draw = draw
+    this.update = update
+
+    this.context = context
+  }
+
+  _update() {
+
+  }
+
+  _draw() {
+    this.context.save()
+
+    this.context.restore()
+  }
+
+  render() {
+    this.draw
+  }
 }
+
+export default Graphic
