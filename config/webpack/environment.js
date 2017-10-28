fb059ca041b94abe2c72f8552f0667289bba1fd6
tylermckenzie <tylrmckenz@gmail.com>
2017-10-27 21:23:10 -0400

    added small webpack stuff

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.set(
+  'Uglify',
+  new webpack.optimize.UglifyJsPlugin({
+    minimize: true,
+    compress: {
+      warnings: false
+    }
+  })
+)
 
 module.exports = environment
